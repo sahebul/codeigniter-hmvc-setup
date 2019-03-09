2e0a6b224331d83931a298d3e50c71932c512e43
sahebul <sahebul99@gmail.com>
2019-03-09 13:07:31 +0530

    first commit

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,12 @@
+<?php
+class MY_Controller extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+        // if (version_compare(CI_VERSION, '2.1.0', '<')) {
+        //     $this->load->library('security');
+        // }
+    }
+
+}
+?>
